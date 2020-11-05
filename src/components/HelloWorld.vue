a3a02f7368a57073b9e14b88d1f90ea9dfa214de
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-11-05 01:18:13 +0100

    layout

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="helloworld">
-    
-  </div>
+  <div  class="helloworld">
+    <div class="daniel set">
+      <img src="../assets/images/image-daniel.jpg" alt="">
+    </div >
+    <div class="jeanette set"></div >
+    <div class="jonathan set"></div >
+    <div class="kira set"></div >
+    <div class="patrick set"></div>
+  </div >
 </template>
 
 <script setup>
