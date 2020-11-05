31766e09aeb3700685f0f8e91a8f069d18ac357f
dan-adekoya <60848550+dan-adekoya@users.noreply.github.com>
2020-11-05 12:03:16 +0100

    firdt grid

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,18 @@
 <template>
   <div  class="helloworld">
     <div class="daniel set">
-      <img src="../assets/images/image-daniel.jpg" alt="">
+      <div class="name">
+        <img src="../assets/images/image-daniel.jpg" alt="">
+        <div class="name_det">
+          <h3>Daniel Clifford</h3 >
+          <span>Verified Graduate</span>
+        </div>
+      </div>
+      <h3>
+        I received a job offer mid-course, and the subjects I learned were current, if not more so, 
+        in the company I joined. I honestly feel I got every penny’s worth.
+      </h3>
+      <img class="bg_quote" src="../assets/images/bg-pattern-quotation.svg" alt="">
     </div >
     <div class="jeanette set"></div >
     <div class="jonathan set"></div >
